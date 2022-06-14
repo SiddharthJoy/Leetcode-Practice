a9cb89fe9ca1467625ebdfd11e6e000bb50fc98c
Siddhartha Barman Joy <siddharthjoy88@gmail.com>
2022-06-15 01:13:52 +0600

    Time: 448 ms (96.07%), Space: 0B (100.00%) - LeetHub

--- 511-game-play-analysis-i/511-game-play-analysis-i.sql
+++ 511-game-play-analysis-i/511-game-play-analysis-i.sql
@@ -1,12 +1,3 @@
-# Write your MySQL query statement below
-Select distinct a.player_id,a.event_date as first_login
-from Activity a, Activity b
-where
-(
-    a.player_id = b.player_id
-    and
-    a.event_date = (select min(event_date) 
-                    from Activity 
-                    where player_id = a.player_id
-                    group by player_id)
-)
+Select player_id, min(event_date) as first_login
+from Activity
+group by player_id
