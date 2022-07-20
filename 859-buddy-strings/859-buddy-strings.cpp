cfadf3e7e1d7a64527ba3c78f89b91199e16eefe
Siddhartha Barman Joy <siddharthjoy88@gmail.com>
2022-07-20 19:25:28 +0600

    Time: 0 ms (100.00%), Space: 6.9 MB (64.10%) - LeetHub

---
+++ 859-buddy-strings/859-buddy-strings.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    bool buddyStrings(string s, string goal) {
+        int cnt = 0;
+        int n = s.size();
+        int nn = goal.size();
+        
+        bool duplicate = false;
+        
+        if(n != nn) return false;
+        
+        if(s == goal){
+            int characters[26];
+            for(int i = 0;i<26;i++) characters[i] = 0;
+            
+            for(int i = 0;i<n;i++){
+                characters[s[i]-'a']++;
+                if(characters[s[i]-'a'] > 1) return true;
+            }
+            
+            return false;
+        }
+        
+        
+        int idx,idxx;
+        idx = idxx = -1;
+        for(int i = 0;i<n;i++){
+            if(s[i] != goal[i]){
+                if(idx == -1) idx = i;
+                else if(idxx == -1) idxx = i;
+                else return false;
+            }
+        }
+        
+        if(idxx > 0 && s[idx] == goal[idxx] && s[idxx] == goal[idx]) return true;
+        
+        return false;
+    }
+};
