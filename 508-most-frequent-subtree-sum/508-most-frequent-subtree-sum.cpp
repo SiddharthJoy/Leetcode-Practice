fc405a52053445efe0088316e362651211f82520
Siddhartha Barman Joy <siddharthjoy88@gmail.com>
2022-07-21 12:09:54 +0600

    Time: 38 ms (25.66%), Space: 24.8 MB (18.74%) - LeetHub

---
+++ 508-most-frequent-subtree-sum/508-most-frequent-subtree-sum.cpp
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    map<int,int> mp;
+    int mx = -1;
+    
+    int dfs(TreeNode* node){
+        if(node == NULL) return 0;
+        int lft = dfs(node -> left);
+        int rgt = dfs(node -> right);
+        
+        int sum = node -> val + (lft + rgt);
+        
+        mp[sum]++;
+        
+        if(mx < mp[sum]) mx = mp[sum];
+        
+        return sum;
+    }
+    
+    vector<int> findFrequentTreeSum(TreeNode* root) {
+        vector<int> v;
+        int x = dfs(root);
+        
+        for(auto it: mp){
+            if(it.second == mx) v.push_back(it.first);
+        }
+        
+        return v;
+    }
+};
