3d9f301adcf2fd9f6e80b171bd765d9f0bea321a
Siddhartha Barman Joy <siddharthjoy88@gmail.com>
2022-08-17 12:29:10 +0600

    Time: 13 ms (13.71%), Space: 12.2 MB (36.78%) - LeetHub

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -1,6 +1,32 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-         next_permutation(nums.begin(),nums.end());
+        int n = nums.size(),idx = -1;
+        
+        for(int i = n-1;i>0;i--){
+            if(nums[i] > nums[i-1]){
+                idx = i-1;
+                break;
+            }
+        }
+        
+        if(idx == -1){
+            for(int i = 0;i<n/2;i++) swap(nums[i],nums[n-i-1]);
+            return;
+        }
+        
+        
+        nums.push_back(-1);
+
+        for(int i = idx + 1;i<n+1;i++){
+            if(nums[idx] >= nums[i]){
+                swap(nums[idx],nums[i-1]);
+                break;
+            }
+        }
+        
+        nums.pop_back();
+        
+        sort(nums.begin()+idx+1,nums.end());
     }
 };
