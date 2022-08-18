7e2f5e43a52d3860fcd7163b177422cd1ab7434b
Siddhartha Barman Joy <siddharthjoy88@gmail.com>
2022-08-18 12:57:11 +0600

    Time: 0 ms (100.00%), Space: 7.2 MB (33.72%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -3,12 +3,12 @@ public:
     void rotate(vector<vector<int>>& matrix) {
     int n = matrix.size();
 
-    int ro = 0, col = 0, x = n - 1;
+    int x = n - 1;
     for (int i = 0; i * 2 <= n; i++) {
         for (int j = i; j < x; j++) {
             swap(matrix[i][j], matrix[j][x]);
-            swap(matrix[i][j], matrix[x][x - j+i]);
-            swap(matrix[i][j], matrix[x - j+i][i]);
+            swap(matrix[i][j], matrix[x][x - j + i]);
+            swap(matrix[i][j], matrix[x - j + i][i]);
         }
         x--;
     }
